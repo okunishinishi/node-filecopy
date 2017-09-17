c7ef7dae7b9aa911f9bd9b244f005f2c0170963d
okunishinishi <okunishinishi@gmail.com>
2017-09-17 11:48:36 +0900

    Version incremented to 4.0.1

--- test/filecopy_test.js
+++ test/filecopy_test.js
@@ -9,39 +9,39 @@ const filecopy = require('../lib/filecopy.js')
 const path = require('path')
 const mkdirp = require('mkdirp')
 const fs = require('fs')
-const co = require('co')
+
 const assert = require('assert')
 
 describe('filecopy', () => {
-  it('Copy a single file with permission.', () => co(function * () {
+  it('Copy a single file with permission.',async () => {
     let src = String(__filename)
     let dest = 'tmp/foo/bar/copy11.txt'
-    yield filecopy(src, dest, {
+    await filecopy(src, dest, {
       mkdirp: true,
       mode: '444'
     })
     assert.ok(fs.existsSync(dest))
-  }))
+  })
 
-  it('Copy a single file.', () => co(function * () {
+  it('Copy a single file.',async () => {
     let src = String(__filename)
     let dest = 'tmp/foo/bar/copy01.txt'
-    yield filecopy(src, dest, {
+    await filecopy(src, dest, {
       mkdirp: true
     })
     assert.ok(fs.existsSync(dest))
-  }))
+  })
 
-  it('Copy multiple files.', () => co(function * () {
+  it('Copy multiple files.',async () => {
     let src = String(__dirname) + '/*.js'
     let dest = 'tmp/foo/bar/baz'
     mkdirp.sync(dest)
-    let results = yield filecopy(src, dest, {
+    let results = await filecopy(src, dest, {
       mkdirp: true
     })
     assert.ok(fs.existsSync(dest + '/' + path.basename(__filename)))
     assert.ok(results)
-  }))
+  })
 })
 
 /* global describe, before, after, it */
--- test/index_test.js
+++ test/index_test.js
@@ -6,15 +6,15 @@
 'use strict'
 
 const index = require('../lib/index')
-const co = require('co')
+
 const assert = require('assert')
 
-it('Eval properties.', () => co(function * () {
+it('Eval properties.', () => {
   assert.ok(index)
   Object.keys(index).forEach((key) => {
     assert.ok(key)
-    assert.ok(index[ key ])
+    assert.ok(index[key])
   })
-}))
+})
 
 /* global it */
